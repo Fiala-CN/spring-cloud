1143003b29c1c54146751155758dee3d70ab65c4
fengyao <fengyao@g2link.cn>
2018-04-03 15:37:58 +0800

    update

---
+++ config-server-8770/src/main/java/com/von/configserver/ConfigServer8770Application.java
@@ -0,0 +1,12 @@
+package com.von.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServer8770Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServer8770Application.class, args);
+	}
+}
---
+++ config-server-8770/src/test/java/com/von/configserver/ConfigServer8770ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.von.configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigServer8770ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
